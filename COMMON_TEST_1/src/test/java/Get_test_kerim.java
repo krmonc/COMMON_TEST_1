95a3e73768506cc1d8d8630f561f2cee9544cfd9
krmonc <67947673+krmonc@users.noreply.github.com>
2020-07-28 06:21:12 -0500

    get test 1 ... kerim

---
+++ COMMON_TEST_1/src/test/java/Get_test_kerim.java
@@ -0,0 +1,19 @@
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import io.restassured.http.ContentType;
+
+
+
+public class Get_test_kerim {
+	@Test
+	public void Test1 () {
+		baseURI = "https://reqres.in";
+		given().get("/api/users").
+		then().statusCode(200).log().all();	}
+	
+	
+
+}
